2f427173b5784c2f11442e827db5af30e727544c
pantilei ianulov <ianulov.pantilei@gmail.com>
2022-09-15 20:19:28 +0300

    [FEAT]: Add department size to departments model

--- custom-addons/restaurant_hr/models/hr_department.py
+++ custom-addons/restaurant_hr/models/hr_department.py
@@ -10,3 +10,16 @@ class HrDepartment(models.Model):
         string='Sequence',
         default=10,
     )
+
+    department_size = fields.Selection(
+        selection=[
+            ("small", "Small"),
+            ("medium", "Medium"),
+            ("large", "Large"),
+        ],
+        string="Department Size",
+        help="""
+            If department is the restaurant, you can specify its size, the salaries of certain employees will 
+            depend on it.
+        """
+    )
