bdcb7aadca0281bedb720086aeb047cf199ea1da
pantilei ianulov <ianulov.pantilei@gmail.com>
2023-06-07 21:36:17 +0300

    [FEAT]: Make fields non translatable in surveys

--- custom-addons/restaurant_hr/models/survey_survey.py
+++ custom-addons/restaurant_hr/models/survey_survey.py
@@ -6,6 +6,14 @@ class SurveySurvey(models.Model):
     _inherit = "survey.survey"
 
     title = fields.Char('Survey Title', required=True, translate=False) 
+    description = fields.Html(
+        "Description", translate=False, sanitize=False,  # TDE FIXME: find a way to authorize videos
+        help="The description will be displayed on the home page of the survey. You can use this to give the purpose and guidelines to your candidates before they start it.")
+    
+    description_done = fields.Html(
+        "End Message", translate=False,
+        help="This message will be displayed when survey is completed")
+    
     wage_rate_waiter_ids = fields.One2many(
         comodel_name="survey.survey_wage_rate",
         inverse_name="survey_id",
@@ -57,7 +65,20 @@ class SurveySurvey(models.Model):
 class SurveyQuestion(models.Model):
     _inherit = "survey.question"
     title = fields.Char('Title', required=True, translate=False)
-    
+    description = fields.Html(
+        'Description', sanitize=False, translate=False,
+        help="Use this field to add additional explanations about your question or to illustrate it with pictures or a video"
+    )
+    comments_message = fields.Char('Comment Message', translate=False, default=lambda self: _("If other, please specify:"))
+    validation_error_msg = fields.Char('Validation Error message', translate=False, default=lambda self: _("The answer you entered is not valid."))
+    constr_error_msg = fields.Char('Error message', translate=False, default=lambda self: _("This question requires an answer."))
+
+
+class SurveyQuestionAnswer(models.Model):
+    _inherit = 'survey.question.answer'
+
+    value = fields.Char('Suggested value', translate=False, required=True)  
+
 
 class SurveySurveyBonuses(models.Model):
     _name = "survey.survey_wage_rate"
